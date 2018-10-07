3de1cefc179174bd3cac27bdf0bc95542fe78ed0
haorengao <872248907@qq.com>
2018-10-07 18:59:53 +0800

    测试

--- src/main/java/com/example/hrgtest/HrgtestApplication.java
+++ src/main/java/com/example/hrgtest/HrgtestApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HrgtestApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(HrgtestApplication.class, args);
 	}
 }
